ba1517e8640610c464b6afec8ac132d56c168a8d
John Mitsch <jomitsch@redhat.com>
2020-08-11 14:29:18 -0400

    Added tailwind styles

--- components/TransactionForm.js
+++ components/TransactionForm.js
@@ -7,6 +7,7 @@ import { useForm } from "react-hook-form";
 
 const TransactionForm = ({ addTransaction }) => {
     const { errors, register, handleSubmit } = useForm();
+    const inputClass = "bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 appearance-none"
 
     const onSubmit = ({ name, amount, price }) => {
       addTransaction({ name, amount, price })
@@ -14,20 +15,39 @@ const TransactionForm = ({ addTransaction }) => {
 
     return (
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
+        <div className="items-center">
           I bought&nbsp;
-          <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal" placeholder="amount" name="amount" type="number" step="any" ref={register({ required: true, min: 0 })} />
+        </div>
+        <div className="items-center">
+          <input className={inputClass} placeholder="amount" name="amount" type="number" step="any" ref={register({ required: true, min: 0 })} />
+        </div>
+        <div className="items-center">
           &nbsp;of&nbsp;
-          <input placeholder="name of cryptocurrency" name="name" type="text" ref={register({ required: true })} />
+        </div>
+        <div className="items-center">
+          <input className={inputClass} placeholder="name of cryptocurrency" name="name" type="text" ref={register({ required: true })} />
+        </div>
+        <div className="items-center">
           &nbsp;at&nbsp;
-          <input placeholder="price at purchase" name="price" type="number"  step="any" ref={register({ required: true, min: 0 })} />
         </div>
-        <div>
-          { errors.name && "Name is required"}
-          { errors.amount && "Amount is required and must be a valid number"}
-          { errors.price && "Price is required and must be a valid number"}
+        <div className="items-center">
+          <input className={inputClass} placeholder="price at purchase" name="price" type="number"  step="any" ref={register({ required: true, min: 0 })} />
         </div>
-        <input type="submit" />
+        <div className="items-center">
+          { errors.name && 
+            <div class="w-full bg-red-100 border border-red-400 text-red-700 my-1 p-2 rounded" role="alert">
+              Name is required
+            </div>}
+          { errors.amount &&
+            <div class="w-full bg-red-100 border border-red-400 text-red-700 my-1 p-2 rounded" role="alert">
+              Amount is required and must be a valid number
+              </div>}
+          { errors.price &&
+            <div class="w-full bg-red-100 border border-red-400 text-red-700 my-1 p-2 rounded" role="alert">
+            Price is required and must be a valid number
+            </div>}
+        </div>
+        <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
       </form>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,34 @@ import Transactions from '../components/Transactions'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Crypto Bull Run Calculator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex items-center justify-center h-screen">
+        <Head>
+          <title>Crypto Bull Run Calculator</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <h1 className="text-lg text-center m-4">
-          Crypto Bull Run Calculator
-        </h1>
+        <main>
+          <h1 className="text-5xl text-center font-extrabold">
+            Crypto Bull Run Calculator
+          </h1>
 
-        <p>
-          Be a smart investor and plan when you sell your cryptocurrencies. Get started by entering a transaction.
-        </p>
+          <p className="text-3xl text-center">
+            Be a smart investor and plan when you sell your cryptocurrencies. Get started by entering a transaction.
+          </p>
 
-        <h3>New Transaction</h3>
-        <Transactions />
-      </main>
+          <div className="flex flex-wrap mb-1 text-center">
+            <div className="w-1/3 px-2"></div>
+            <div className="w-1/3 ml-auto px-2">
+              <h3 className="text-lg font-bold">New Transaction</h3>
+            </div>
+            <div className="w-1/3 px-2"></div>
+            <div className="w-1/3 px-2"></div>
+            <div className="w-1/3 px-2 bg-gray-200 rounded p-4">
+              <Transactions />
+            </div>
+            <div className="w-1/3 px-2"></div>
+          </div>
+        </main>
     </div>
   )
 }
